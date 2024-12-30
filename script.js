10127d3b50941da383a837f437918448aa3797c8
Daniel Gradonski <daniel.gradonski@gmail.com>
2024-12-30 18:31:43 +0100

    Poprawienie systemu wyświetlania Usuwanie ostatniego elementu Dodanie funkcji do klawiszy specjalnych

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const upperDisplay = document.getElementById("displayUpper");
 const lowerDisplay = document.getElementById("displayLower");
-
 const buttons = document.querySelectorAll("button");
+
 // Dodanie listener do każdego przycisku / wyświetlanie danych na lowerDisplay
 buttons.forEach((button) => {
     button.addEventListener("click", event => {
         displayFunc(event);
+        specialKeys(event);
     }
     );
-})
+});
 
+// Wyświetla tylko liczby w razie innego klawisza pomija
 function displayFunc(event) {
-    lowerDisplay.innerText += event.target.innerText;
-}
+    if (event.target.innerText !== "DELETE" && event.target.innerText !== "CLEAR") {
+        lowerDisplay.innerText += event.target.innerText;
+    };
+
+};
+
+//funkcja określająca klawisze specjalne i co ma zostać wykonane po ich naciśnięciu
+function specialKeys(event) {
+    if (event.target.innerText === "DELETE") {
+        lowerDisplay.innerText = "";
+        valueA = "";
+    };
+    if (event.target.innerText === "CLEAR") {
+        lowerDisplay.innerText = lowerDisplay.innerText.slice(0, -1);
+    };
+
+};
