7e45d71ec060b74e19bcae81e4f84b0dac8ff1f7
Daniel Gradonski <daniel.gradonski@gmail.com>
2024-12-30 18:38:51 +0100

    Przygotowanie programu pod funkcję wyszukiwania zmiennych Poptawiony display i klawisze specjalne

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const upperDisplay = document.getElementById("displayUpper");
 const lowerDisplay = document.getElementById("displayLower");
 const buttons = document.querySelectorAll("button");
 
+let value;
+
 // Dodanie listener do każdego przycisku / wyświetlanie danych na lowerDisplay
 buttons.forEach((button) => {
     button.addEventListener("click", event => {
@@ -13,8 +15,9 @@ buttons.forEach((button) => {
 
 // Wyświetla tylko liczby w razie innego klawisza pomija
 function displayFunc(event) {
-    if (event.target.innerText !== "DELETE" && event.target.innerText !== "CLEAR") {
+    if (event.target.innerText !== "DELETE" && event.target.innerText !== "CLEAR" && event.target.innerText !== "=") {
         lowerDisplay.innerText += event.target.innerText;
+        chceckingValue(event); // <= Muszę dodać funkcję która zbiera zmienne!!!
     };
 
 };
@@ -28,5 +31,8 @@ function specialKeys(event) {
     if (event.target.innerText === "CLEAR") {
         lowerDisplay.innerText = lowerDisplay.innerText.slice(0, -1);
     };
-
+    if (event.target.innerText === "=") { // !! Musi zostać zamienione na funkcję
+        upperDisplay.innerText = lowerDisplay.innerText;
+        lowerDisplay.innerText = "";
+    }
 };
