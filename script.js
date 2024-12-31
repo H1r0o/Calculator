b44a2f1ac692446b95dad69f7c0af7f486b11137
Daniel Gradonski <daniel.gradonski@gmail.com>
2024-12-31 10:51:41 +0100

    Dodanie funkcji która wykrywa rodzaj działania

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ buttons.forEach((button) => {
 function displayFunc(event) {
     if (event.target.innerText !== "DELETE" && event.target.innerText !== "CLEAR" && event.target.innerText !== "=") {
         lowerDisplay.innerText += event.target.innerText;
-        chceckingValue(event); // <= Muszę dodać funkcję która zbiera zmienne!!!
+
     };
 
 };
@@ -33,6 +33,37 @@ function specialKeys(event) {
     };
     if (event.target.innerText === "=") { // !! Musi zostać zamienione na funkcję
         upperDisplay.innerText = lowerDisplay.innerText;
+        value = lowerDisplay.innerText;
+        chceckingValue(value); // <= Muszę dodać funkcję która zbiera zmienne!!!
         lowerDisplay.innerText = "";
+        value = "";
     }
 };
+// Funkcja wykrywa jaki to jest rodzaj działania
+function chceckingValue(zmienna) {
+    if (zmienna.includes("+")) {
+        console.log("Dodawanie");
+    } else if (zmienna.includes("-")) {
+        console.log("Odejmowanie");
+    } else if (zmienna.includes("*")) {
+        console.log("Mnożenie");
+    } else if (zmienna.includes("/")) {
+        console.log("Dzielenie");
+    }
+}
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+}
